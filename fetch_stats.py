7f96de482129babd42ae5d75a431bf12cd0f7546
dmcauley52 <156609879+dmcauley52@users.noreply.github.com>
2026-04-28 17:12:25 -0700

    Update is_starter

--- fetch_stats.py
+++ fetch_stats.py
@@ -60,18 +60,19 @@ for player in qualified:
             team = game.get("team", {}).get("name", "")
             positions = game.get("positionsPlayed", [{}])
             position = positions[0].get("abbreviation", "") if positions else ""
+            is_starter = bool(s.get("gamesStarted", 0))
 
             cur.execute("""
                 INSERT INTO player_gamelogs
                 (player_id, player_name, game_date, season, at_bats, hits,
-                 home_runs, rbi, batting_avg, ops, team, position)
-                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-                ON CONFLICT (player_id, game_date) DO NOTHING
+                 home_runs, rbi, batting_avg, ops, team, position, is_starter)
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                ON CONFLICT (player_id, game_date) DO UPDATE SET is_starter = EXCLUDED.is_starter
             """, (pid, name, gdate, SEASON,
                   s.get("atBats", 0), s.get("hits", 0),
                   s.get("homeRuns", 0), s.get("rbi", 0),
                   s.get("avg", 0), s.get("ops", 0),
-                  team, position))
+                  team, position, is_starter))
 
         conn.commit()
         time.sleep(0.3)
